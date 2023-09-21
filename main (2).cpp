503a6bca028e2250df7df77a48603b4adc0cd53e
Zelles Zalán <zelle@strat-ig.vasiviz.hu>
2023-09-21 15:19:46 +0100

    commentek hozzáadása

--- main (2).cpp
+++ main (2).cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];  // hibás: N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' // Végérõl hiányzik: ;
+    for (int i = 0;) // Zárójelen belül hiányzik: i; i++
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //Végérõl hiányzik: ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) // , helyett ;
     {
-        atlag += b[i]
+        atlag += b[i] // hiányzik végérõl: ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
